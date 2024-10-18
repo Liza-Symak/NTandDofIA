f92ff1fec586e3a138f245e5d6175f5d51a82f4e
Liza Symak 542 <lizasumak2303@gmail.com>
2024-10-18 08:40:00 +0200

    Another line2

--- Laba1.java
+++ Laba1.java
@@ -2,5 +2,6 @@ class Lab1 {
     public static void main(String[] args) {
         System.out.println("It works");
         System.out.println("Another line added");
+        System.out.println("Another line2 ");
     }
 }
