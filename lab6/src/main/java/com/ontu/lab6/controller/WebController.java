e191044e7e27e0d8c784456a814469e09459daa2
Liza Symak 542 <lizasumak2303@gmail.com>
2024-11-14 07:44:46 +0100

    Lab6

---
+++ lab6/src/main/java/com/ontu/lab6/controller/WebController.java
@@ -0,0 +1,23 @@
+package com.ontu.lab6.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WebController {
+
+    @RequestMapping(value = "/index")
+    public String index(Model model) {
+
+        String[][] gameBoard = {
+            {"X", "O", "X"},
+            {"O", "X", " "},
+            {" ", "O", "X"}
+        };
+
+        model.addAttribute("gameBoard", gameBoard);
+
+        return "index";
+    }
+}
